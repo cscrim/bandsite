33f7afde2beeee15aba9ffd3a6c54dd544930462
Courtney S <courtneyscrimshaw@gmail.com>
2024-10-19 09:35:14 -0400

    append API key to axios requests

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,3 +1,4 @@
+const apiKey = "8eb3eebf-5e76-465c-8fb1-e9151d0ac00b";
 
 
 class BandSiteApi {
@@ -8,7 +9,7 @@ class BandSiteApi {
 
     async postComment (comment) {
         try {
-            const response = await axios.post(`${this.baseurl}/comments`, comment);
+            const response = await axios.post(`${this.baseurl}/comments?api_key=${this.apiKey}`, comment);
             return response.data;
 
         } catch (error) {
@@ -16,12 +17,29 @@ class BandSiteApi {
         }
     }
 
-    async getComment () {
+    async getComments() {
         try {
-            const response = await axios.get(`${this.baseurl}/comments`);
-            return response.data;
+            const response = await axios.get(`${this.baseurl}/comments?api_key=${this.apiKey}`);
+
+            const comments = response.data;
+
+            comments.sort((a,b) => new Date(b.date) - new Date(a.date));
+            return comments;
+
         } catch (error) {
             console.log("there was an error getting comments", error);
         }
     }
+
+    async getShows() {
+        try {
+            const response = await axios.get(`${this.baseurl}/shows?api_key=${this.apiKey}`);
+            return response.data;
+        } catch (error) {
+            console.log("there was an error getting shows", error);
+        }
+
+    }
 }
+
+const bandApi = new BandSiteApi(apiKey);
